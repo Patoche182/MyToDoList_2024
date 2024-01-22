7a93b1220aebb0b29ebb4859552c194cc4d19683
PatriceHANNE <patrice.hanne@gmail.com>
2024-01-22 23:58:48 +0100

    Ajoutez des fichiers projet.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace MyToDoList_2024
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
